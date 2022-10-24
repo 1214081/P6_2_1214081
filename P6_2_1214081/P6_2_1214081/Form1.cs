afca6bc462fc3737c021b78c109a6c87140c59b6
Ibrohim Mubarok <114393418+1214081@users.noreply.github.com>
2022-10-24 14:01:36 +0700

    Add files via upload

---
+++ P6_2_1214081/P6_2_1214081/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P6_2_1214081
+{
+    public partial class Form1 : Form
+    {
+        private string filePath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DialogResult userResponse = openFileDialog1.ShowDialog();
+                if(userResponse == DialogResult.OK)
+                {
+                    filePath = openFileDialog1.FileName.ToString();
+                    MessageBox.Show("Sucessfuly open: '" + filePath + "'", "Sucess",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                }
+                else
+                {
+                    MessageBox.Show("Cancel open file", "Warning",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+    }
+}
